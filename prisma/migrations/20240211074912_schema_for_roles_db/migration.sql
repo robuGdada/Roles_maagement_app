2de31139dd98f4556dc3ec95a93edf8181c160df
robuGdada <paudelronish@gmail.com>
2024-02-11 18:17:34 +0545

    fix migration and schema

--- prisma/migrations/20240211074912_schema_for_roles_db/migration.sql
+++ prisma/migrations/20240211074912_schema_for_roles_db/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE `users` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `username` VARCHAR(50) NOT NULL,
-    `passwword` VARCHAR(200) NOT NULL,
+    `password` VARCHAR(200) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
     `roleId` INTEGER NOT NULL,
