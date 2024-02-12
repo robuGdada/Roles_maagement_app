503f6d68e4015c7693b3368e88f3eecfe660047d
robuGdada <paudelronish@gmail.com>
2024-02-12 15:01:57 +0545

    login fileadd

--- form.ts
+++ form.ts
@@ -105,6 +105,7 @@ export const editHtml = ({ id, title, description }: IFormProps) => {
       <button
         class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
         type="submit"
+        id="submit"
       >
         Submit
       </button>
@@ -113,3 +114,40 @@ export const editHtml = ({ id, title, description }: IFormProps) => {
   
   `;
 };
+
+export function loginHtml() {
+  return `
+  <form
+  hx-post="/signin"
+  hx-trigger="submit"
+  hx-vals="username"
+  class="bg-white shadow-2xl rounded px-8 pt-6 pb-8 w-[450px]"
+  hx-swap="outerHTML"
+  hx-target="#todos-container"
+  hx-redirect="/todos"
+>
+  <div class="mb-4">
+    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+      Username
+    </label>
+    <input
+      class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      id="username"
+      name="username"
+      type="text"
+      placeholder="Enter your username"
+    />
+  </div>
+  <div class="flex items-center justify-between">
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      type="submit"
+      id="submit"
+    >
+      login
+    </button>
+  </div>
+</form>
+
+`;
+}
