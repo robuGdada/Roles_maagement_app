b8a0559b197567f98f25455f2f3ba70389468040
robuGdada <paudelronish@gmail.com>
2024-02-14 19:00:10 +0545

    user create role assign middle fix and add

--- resources/users/user.controller.ts
+++ resources/users/user.controller.ts
@@ -1,11 +1,11 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { userRepo } from "./user.repo";
-import { PrismaClient, UserRole } from "@prisma/client";
-import { roleAndPermission } from "./role.permission";
+import { PrismaClient, User } from "@prisma/client";
+
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-
-const prisma = new PrismaClient();
+import { prisma } from "../../server";
+import { permissionRole } from "./role.permission";
 
 const getAll = async (req: Request, res: Response) => {
   const currentPage = Number(req.query.page) || 1;
@@ -16,7 +16,7 @@ const getAll = async (req: Request, res: Response) => {
   try {
     if (!searchVal) {
       const users = await userRepo.getAll(offset, pageSize);
-      // console.log("search val chaina", { users });
+
       return res.json(users);
     } else {
       const users = await userRepo.getAllWithSearch(
@@ -33,79 +33,10 @@ const getAll = async (req: Request, res: Response) => {
 };
 
 //createUser
-
-// const createUser = async (req: Request, res: Response) => {
-//   const { username, password, role } = req.body;
-//   const userData = { username, password, role };
-//   console.log({ userData });
-//   try {
-//     const existingUser = await prisma.user.findFirst({
-//       where: { username },
-//     });
-
-//     if (existingUser) {
-//       return res.status(400).json({
-//         errorType: "User_already_exists",
-//         message: "User already exists",
-//       });
-//     }
-
-//     const roleToFind = role as UserRole;
-//     console.log("Role to Find:", roleToFind);
-
-//     let roleFound = await prisma.roles.findFirst({
-//       where: { roleName: roleToFind },
-//     });
-
-//     console.log("Role Found:", roleFound);
-//     if (!roleFound) {
-//       return res.status(400).json({
-//         errorType: "Role_not_found",
-//         message: `Role not found for ${role}`,
-//       });
-//     } else {
-//       await roleAndPermission({ prisma, roleFound });
-//       const salt = bcrypt.genSaltSync(10);
-//       const hash = await bcrypt.hash(password, salt);
-//       const createdUser = await prisma.user.create({
-//         data: {
-//           username,
-//           password: hash,
-//           role: {
-//             create: {
-//               roleName: role as UserRole,
-//             },
-//           },
-//         },
-//       });
-//       const { sign } = jwt;
-//       const token = sign(
-//         {
-//           username,
-//           id: createdUser.id,
-//         },
-//         process.env.JWT_SECRET_KEY!
-//       );
-
-//       console.log({ hash, token });
-//       return res.status(201).json({
-//         message: "User created successfully",
-//         user: createdUser,
-//         token,
-//       });
-//     }
-//   } catch (error) {
-//     console.error(`Error creating user: ${error}`);
-//     return res.status(500).json({
-//       errorType: "Internal_server_error",
-//       message: "Internal Server Error",
-//     });
-//   }
-// };
-
 const createUser = async (req: Request, res: Response) => {
-  const { username, password, role } = req.body;
-
+  const { username, password, roleId } = req.body;
+  const userData = { username, password };
+  console.log({ userData });
   try {
     const existingUser = await prisma.user.findFirst({
       where: { username },
@@ -117,46 +48,35 @@ const createUser = async (req: Request, res: Response) => {
         message: "User already exists",
       });
     }
-    const roleFound = await prisma.roles.findFirst({
-      where: { roleName: role },
-    });
-    if (!roleFound) {
-      return res.status(400).json({
-        errorType: "Role_not_found",
-        message: `Role not found for ${role}`,
-      });
-    } else {
-      await roleAndPermission({ prisma, roleFound });
-      const salt = bcrypt.genSaltSync(10);
-      const hash = await bcrypt.hash(password, salt);
-      const createdUser = await prisma.user.create({
-        data: {
-          username,
-          password: hash,
-          role: {
-            create: {
-              roleName: role as UserRole,
-            },
-          },
-        },
-      });
-
-      const { sign } = jwt;
-      const token = sign(
-        {
-          username,
-          id: createdUser.id,
-        },
-        process.env.JWT_SECRET_KEY!
-      );
-
-      console.log({ hash, token });
-      return res.status(201).json({
-        message: "User created successfully",
-        user: createdUser,
-        token,
-      });
+    if (roleId) {
+      permissionRole(roleId);
     }
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = await bcrypt.hash(password, salt);
+    const createdUser = await prisma.user.create({
+      data: {
+        username,
+        password: hash,
+        roleId,
+      },
+    });
+
+    const { sign } = jwt;
+    const token = sign(
+      {
+        username,
+        id: createdUser.id,
+      },
+      process.env.JWT_SECRET_KEY!
+    );
+
+    console.log({ hash, token });
+    return res.status(201).json({
+      message: "User created successfully",
+      user: createdUser,
+      token,
+    });
   } catch (error) {
     console.error(`Error creating user: ${error}`);
     return res.status(500).json({
@@ -166,9 +86,141 @@ const createUser = async (req: Request, res: Response) => {
   }
 };
 
-export default createUser;
+const signin = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  console.log({ username });
+  try {
+    const existingUser = await userRepo.getOneUser(username);
 
+    if (!existingUser) {
+      return res
+        .status(400)
+        .json({ errorType: "USER_NOT_FOUND", message: "User not found." });
+    }
+
+    if (!existingUser) {
+      return res.status(400).json({
+        errorType: "USER_NOT_FOUND",
+        message: "USER NOT FOUND",
+      });
+    }
+    const matchPassword = bcrypt.compareSync(
+      password,
+      String(existingUser?.password)
+    );
+    if (!matchPassword) {
+      return res.status(400).json({
+        errorType: "INVALID_CREDENTIALS",
+        message: "Invalid credentials.",
+      });
+    }
+    const { sign } = jwt;
+    const token = sign(
+      { username: existingUser?.username, id: existingUser?.id },
+      process.env.JWT_SECRET_KEY!
+    );
+    console.log({ existingUser });
+    return res.status(200).json({ user: existingUser, token });
+  } catch (e) {
+    console.log(e);
+    return res
+
+      .status(400)
+      .json({ errorType: "USER_NOT_FOUND", message: "User not found." });
+  }
+};
+
+//middleware for userauth
+export const verifyUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers["authorization"];
+
+  if (!token) {
+    return res.status(401).json({ message: "No token provided." });
+  }
+  try {
+    console.log("token in middleware:", token);
+    const { verify } = jwt;
+    const decoded: any = verify(token, process.env.JWT_SECRET_KEY!);
+
+    console.log({ decoded });
+    const user = await userRepo.getOneUser({ id: decoded.id });
+    req.authUser = user as User;
+    if (user) {
+      next();
+      // return res.status(201).json({ message: "nice intiative" });
+    } else {
+      return res.status(401).json({ message: "token invalid." });
+    }
+  } catch (e) {
+    return res.status(401).json({ message: "token invalid." });
+    // console.log(e);
+  }
+};
+//middleware for Permission Verify
+
+type UserPermission = {
+  permission: {
+    action: string;
+  };
+};
+
+export const permissionVerify = (requiredActions: string[]) => {
+  return async (
+    req: Request & { authUser: { roleId: number } },
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const { roleId } = req.authUser;
+
+      const userPermissions: UserPermission[] =
+        await prisma.permissionOnRole.findMany({
+          where: {
+            roleId,
+          },
+          select: {
+            permission: {
+              select: {
+                action: true,
+              },
+            },
+          },
+        });
+
+      // Extract action strings from the result
+      const userActionStrings = userPermissions.map((p) => p.permission.action);
+
+      // Check if all required actions are present in the user's permissions
+      const allActionsPresent = requiredActions.every((a) =>
+        userActionStrings.includes(a)
+      );
+
+      if (allActionsPresent) {
+        next();
+      } else {
+        return res.status(403).json({
+          errorType: "Permission_denied",
+          message: `Permission denied. User does not have all of the required permissions (${requiredActions.join(
+            ", "
+          )}).`,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({
+        errorType: "Internal_server_error",
+        message: "Internal Server Error",
+      });
+    }
+  };
+};
 export const userController = {
   getAll,
   createUser,
+  signin,
+  verifyUser,
 };
