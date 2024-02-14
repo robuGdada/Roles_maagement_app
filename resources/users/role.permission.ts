4310cd9607352c77ddca131c3b7b32fde7c70dde
robuGdada <paudelronish@gmail.com>
2024-02-14 17:52:38 +0545

    assign permission fn

--- resources/users/role.permission.ts
+++ resources/users/role.permission.ts
@@ -1,71 +1,18 @@
-import { PrismaClient, Roles } from "@prisma/client";
+import { prisma } from "../../server";
 
-export async function roleAndPermission({
-  prisma,
-  roleFound,
-}: {
-  prisma: PrismaClient;
-  roleFound: Roles;
-}) {
-  try {
-    const existingPermissions = await prisma.permissions.findUnique({
-      where: {
-        roleId: roleFound.id,
-      },
-    });
-    console.log(
-      `Current permissions for role ${roleFound.roleName}:`,
-      existingPermissions
-    );
+export const permissionRole = async (roleId: number) => {
+  if (roleId === 1) {
+    const permissionsToGrant = [2, 3];
 
-    let updateData;
-
-    if (roleFound.roleName === "ADMIN") {
-      updateData = { create: true, delete: true, read: true, update: true };
-    } else if (roleFound.roleName === "MOD") {
-      updateData = { create: true, delete: false, read: true, update: true };
-    } else {
-      updateData = { create: true, delete: false, read: true, update: false };
-    }
-
-    if (existingPermissions) {
-      await prisma.permissions.update({
-        where: {
-          id: existingPermissions.id,
-        },
-        data: updateData,
-      });
-
-      console.log(
-        `Permissions updated for role ${roleFound.roleName}:`,
-        updateData
-      );
-    } else {
-      const createdPermissions = await prisma.permissions.create({
-        data: {
-          ...updateData,
-          role: {
-            connect: { id: roleFound.id },
+    await Promise.all(
+      permissionsToGrant.map(async (permissionId) => {
+        await prisma.permissionOnRole.create({
+          data: {
+            roleId: roleId,
+            permissionId,
           },
-        },
-      });
-
-      console.log(
-        `Permissions created for role ${roleFound.roleName}:`,
-        createdPermissions
-      );
-    }
-
-    const updatedPermissions = await prisma.permissions.findUnique({
-      where: {
-        roleId: roleFound.id,
-      },
-    });
-    console.log(
-      `Updated permissions for role ${roleFound.roleName}:`,
-      updatedPermissions
+        });
+      })
     );
-  } catch (error) {
-    console.error(`Error updating/creating permissions: ${error}`);
   }
-}
+};
