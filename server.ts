9a7d532bd43ed65ee21c8a4c0c0c6e93f39a8771
robuGdada <paudelronish@gmail.com>
2024-02-20 10:04:36 +0545

    seeding of permission

--- server.ts
+++ server.ts
@@ -1,6 +1,8 @@
 import { app } from "./app";
+import { seeding } from "./seeding/seed";
 const Port = process.env.PORT;
 app.listen(Port, () => {
   // seeding();
+
   console.log(`Server running at ${Port}`);
 });
