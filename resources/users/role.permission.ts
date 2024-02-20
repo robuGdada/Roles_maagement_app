f6f0b1e9e766c981508560ab051d76f2e6b89466
robuGdada <paudelronish@gmail.com>
2024-02-20 10:05:08 +0545

    chore

--- resources/users/role.permission.ts
+++ resources/users/role.permission.ts
@@ -3,7 +3,6 @@ import { prisma } from "../../app";
 export const permissionRole = async (roleId: number) => {
   if (roleId === 1) {
     const permissionsToGrant = [2, 3];
-
     await Promise.all(
       permissionsToGrant.map(async (permissionId) => {
         await prisma.permissionOnRole.create({
